d650469637c10e56c7728cbbb59cfefeb43ab1fb
Ivan Andrade da Silva <i.andrade@aluno.uepb.edu.br>
2025-06-11 22:56:34 -0300

    Corrigindo as pastas e arquivos do projeto

--- routes/rotas.js
+++ routes/rotas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const { convertFile } = require('../controllers/convertController');
+const { convertFile } = require('../controllers/Controllers.js');
 
 const router = express.Router();
 
